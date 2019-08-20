b7e74e66e8fe76d06a208a2b7b36a29edce53ad6
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-08-21 00:02:21 +0300

    topsym.sh: symbols in subpackages contribute sqrt(n)
    If a symbol is referenced a few times in a package (in different files),
    it counts as one.  This is consistent with the upcoming data format
    which stores the unique list of symbols per package.  Furthermore,
    we want to downvote the symbols which get high counts due to many
    subpackages originating from a single srpm.  To achieve this, we run
    rpmelfsym.pl two times and sum unique counts per subpackage and per srpm
    (in a sense averaging the two).  I noticed though that this is not good
    enough.  For example, you can see many _TRACE_QEMU* symbols on the list,
    originating from qemu-* subpackages, 150 of them!
    
    Now, why do we want to downvote the symbols?  They generate the same
    hashing costs, etc, but they are package-specific and they may go away
    without notice (or renamed into something else).  So it's wrong to
    conclude that _TRACE_QEMU* are widely used symbols just because qemu
    has been sliced into too many subpackages.  Tomorrow they may vanish
    into thin air, and the slot they took remains wasted.  As the Bible
    says, fire came down from heaven and consumed the captain of fifty
    with his fifty!
    
    So when we see such a symbol whose high count is due to many
    subpackages, we should be more willing to consider the alternatives
    whose raw counts are almost as good but srpm counts are much better.
    To fit something like this into the existing scheme, this commit does
    the following:
    
    - Symbols are counted once per package, as previously.
    - We then count how many subpackages reference a symbol, per srpm.
      However, we limit its contribution to the total count: only sqrt(n),
      per srpm, are summed up.

--- topsym.sh
+++ topsym.sh
@@ -2,7 +2,6 @@
 
 DumpDir()
 {
-    rpmelfsym.pl "$1"
     rpm2srpm=rpm2srpm.${1//\//_}
     if [ ! -s "$rpm2srpm" ] || [ "$rpm2srpm" -nt "$1" ] || [ "$rpm2srpm" -ot "$1" ]; then
 	find "$1" -mindepth 1 -maxdepth 1 -name '*.rpm' -execdir \
@@ -11,7 +10,7 @@ DumpDir()
 	touch -r "$1" "$rpm2srpm"
     fi
     rpmelfsym.pl "$1" |
-	join -t$'\t' -o '1.2 2.2 2.3 2.4' "$rpm2srpm" -
+	join -t$'\t' -o '1.2 1.1 2.3 2.4' "$rpm2srpm" -
 }
 
 ProcDump()
@@ -25,11 +24,41 @@ ProcDump()
 	$4 != "_end"              &&
 	$4 != "_fini"             &&
 	$4 != "_init" {
-	    print $1 "\t" $4
+	    print $1 "\t" $2 "\t" $4
 	}
     ' |
-    sort -u |cut -f2- |
-    sort |uniq -c |awk '$1>2'
+    sort -t$'\t' -u -k3 -k1,2 |
+    awk -F'\t' '
+	function flushSym()
+	{
+	    N += sqrt(n)
+	    if (N > 1.7) # at least two srpms, or at least three subpackages
+		print N "\t" sym
+	}
+	{   if (sym == $3) {
+		if ($1 == srpm)
+		    n++; # the same srpm, count subpackages with this symbol
+		else {
+		    # combine subpackage count, start another srpm
+		    N += sqrt(n)
+		    srpm = $1
+		    n = 1
+		}
+	    }
+	    else {
+		# another symbol, another dollar
+		if (n)
+		    flushSym()
+		sym = $3
+		srpm = $1
+		N = 0; n = 1
+	    }
+	}
+	END {
+	    if (n)
+		flushSym()
+	}
+    '
 }
 
 for d; do
@@ -41,22 +70,20 @@ done
 shift $(($#/2))
 
 sort -m -k2 "$@" |
-awk '
+awk -F'\t' '
     function printSym()
     {
 	ShortLen = 16
-	N *= log(ShortLen + length(SYM))
-	print N "\t" SYM
+	N *= log(ShortLen + length(sym))
+	print N "\t" sym
     }
-    {   n = $1
-	sub(/^ *[1-9][0-9]* /, "")
-	if ($0 == SYM)
-	    N += n
+    {   if ($2 == sym)
+	    N += $1
 	else {
 	    if (N)
 		printSym()
-	    SYM = $0
-	    N = n
+	    sym = $2
+	    N = $1
 	}
     }
     END {
