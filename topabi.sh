04e7993cefc346dee9ba0f698c3ef77b564ad822
Alexey Tourbin <alexey.tourbin@gmail.com>
2021-05-13 14:58:04 +0300

    topabi.sh topabi.list: initial revision
    $ ./topabi.sh /ALT/{p9/aarch64,Sisyphus/ppc64le}/RPMS.classic \
                  /klad/Fedora{31/armhfp,32/aarch64,34/s390x}/os/Packages \
              0.9 /ALT/{p9/i586,Sisyphus/x86_64}/RPMS.classic \
                  /klad/Fedora33/x86_64/os/Packages
    
    256 items on the list seem enough.  Items on the list can be referenced
    in a straightforward way (via uint8_t[]).  The ratio of the "second half",
    how much another 256 items can buy us, is low:
    
    $ tail -512 abi.1k |head -256 |cut -f1 |Sum
    2807.55221
    $ tail -512 abi.1k |tail -256 |cut -f1 |Sum
    92880.349
    $ perl -le 'print 2807.55221/92880.349'
    0.0302276233910361
    
    So the ratio is about 3%.  For comparison, when going from 64k symbols
    to 128k symbols, the ratio is about 15%.
    
    $ tail -$((128<<10)) out.1M |head -$((64<<10)) |cut -f1 |Sum
    839042.405520007
    $ tail -$((128<<10)) out.1M |tail -$((64<<10)) |cut -f1 |Sum
    5511649.71329999
    $ perl -le 'print 839042.405520007/5511649.71329999'
    0.152230720231611
    
    libjson-glib-1.0.so.0 is a strange business.  It is specific to recent
    Fedora releases.
    
    $ fgrep -w libjson-glib-1.0.so.0 abi._*
    abi._klad_Fedora33_x86_64_os_Packages:49.4745   libjson-glib-1.0.so.0
    abi._klad_Fedora34_s390x_os_Packages:137.914    libjson-glib-1.0.so.0
    
    $ rpmpeek /klad/Fedora34/s390x/os/Packages/j/json-glib-1.6.2-1.fc34.s390x.rpm \
      objdump -p ./usr/lib64/libjson-glib-1.0.so.0 |awk '/^Version [Dd]ef/,/^$/'
    Version definitions:
    1 0x01 0x04202610 libjson-glib-1.0.so.0
    2 0x00 0x04202610 libjson-glib-1.0.so.0
    
    $ rpmpeek /klad/Fedora34/s390x/os/Packages/j/json-glib-1.6.2-1.fc34.s390x.rpm \
      readelf --wide --dyn-sym ./usr/lib64/libjson-glib-1.0.so.0 |grep -m1 @lib
       264: 0000000000011f80   194 FUNC    GLOBAL DEFAULT   12 json_object_get_int_member_with_default@@libjson-glib-1.0.so.0
    
    What's on the boundary:
    
    $ tail -$((256+8)) abi.1k |head -16
    17.9293 TELEPATHY_GLIB_0.11.6
    18.0142 NSS_3.7
    18.0616 SUNWprivate_1.1
    18.1012 LIBXML2_2.6.20
    18.1465 GLIBC_2.13
    18.1691 OPENCL_1.0
    18.223  TELEPATHY_GLIB_0.7.2
    18.223  TELEPATHY_GLIB_0.7.9
    18.2687 LIBXML2_2.6.17
    18.3623 TELEPATHY_GLIB_0.7.17
    18.4804 TDB_1.2.1
    18.4861 LIBNSL_1.0
    18.5392 LIBSCIM_1.0
    18.6495 GFAPI_3.4.0
    18.7718 LIBVIRT_0.4.0
    18.9988 LIBPROTOBUF_C_1.0.0
    
    So TELEPATHY_GLIB_0.7.17 gets a boost because it's a longer string, and
    ends up on the list, while NSS_3.7 does not.  Raw counts are slightly
    higher for NSS_3.7.
    
    $ awk '$2=="NSS_3.7"{print$1}' abi._* |Sum
    128.1534
    $ awk '$2=="TELEPATHY_GLIB_0.7.17"{print$1}' abi._* |Sum
    112.96

--- topabi.sh
+++ topabi.sh
@@ -17,50 +17,27 @@ DumpDir()
 	rpmquery --qf '%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}.rpm\t%{SOURCERPM}\n' -p '{}' '+' |
 	sort -u >"$rpm2srpm"
     fi
-    if [ "${subdirs:-0}" -eq 0 ]; then
-	rpmelfsym.pl "$1"
-    else
-	cd "$1"; set +f # subshell
-	ls */*.rpm |sort -t/ -k2 |xargs --delimiter='\n' \
-	rpmelfsym.pl
-    fi |
-	join -t$'\t' -o '1.2 1.1 2.2 2.3 2.4' "$rpm2srpm" -
+    find "$1" -mindepth $depth -maxdepth $depth -name '*.rpm' |
+    while read -r f; do
+	rpmelfsym.pl "$f" </dev/null |cut -f2 |
+	sort -u |sed '\|^/usr/share/doc/|d;s/^/./' |
+	rpmpeek "$f" xargs -r --delimiter='\n' objdump -p |
+	awk -v rpm="${f##*/}" '
+	    /^Version [Dd]ef/,/^$/ { if (NF==4 && +$1>1) print rpm "\tA\t" $NF }
+	    /^Version [Rr]ef/,/^$/ { if (NF==4 && / 0x/) print rpm "\tU\t" $NF }'
+    done |
+	sort -u |
+	join -t$'\t' -o '1.2 1.1 2.2 2.3' "$rpm2srpm" -
 }
 
 ProcDump()
 {
-    awk -F'\t' '
-	BEGIN {
-	    Junk["__bss_end__"]
-	    Junk["__bss_start"]
-	    Junk["__bss_start__"]
-	    Junk["__data_start"]
-	    Junk["__end__"]
-	    Junk["__gmon_start__"]
-	    Junk["__libc_csu_fini"]
-	    Junk["__libc_csu_init"]
-	    Junk["_bss_end__"]
-	    Junk["_edata"]
-	    Junk["_end"]
-	    Junk["_fini"]
-	    Junk["_init"]
-	    Junk["_start"]
-	    Junk["data_start"]
-	    Junk["main"]
-	}
-	index("UTWVDBRuiGS", $4) &&
-	!($5 in Junk) &&
-	index($3, "/usr/share/doc/") != 1 {
-	    print $1 "\t" $2 "\t" $4 "\t" $5
-	}
-    ' |
     sort -t$'\t' -u -k4 -k1,3 --compress-program=lz4 |
     awk -F'\t' '
 	function flushSym()
 	{
 	    N += (n ** 0.63) * (allU ? 1.28 : 1)
-	    if (N > 1.99) # at least two srpms, or at least three subpackages
-		print N "\t" sym
+	    print N "\t" sym
 	}
 	{   if (sym == $4) {
 		if ($1 == srpm && $2 == rpm) {
@@ -104,7 +81,7 @@ for d; do
 	continue
     fi
     d=${d%/}
-    out=out.${d//\//_}
+    out=abi.${d//\//_}
     [ -s "$out" ] ||
     DumpDir "$d" |ProcDump >$out
     if [ $w = 1.0 ]; then
@@ -125,10 +102,6 @@ awk -F'\t' -v NFiles=$wsum '
     function printSym()
     {
 	N *= log(ShortLen + length(sym))
-	if (match(sym, /^caml.*_([0-9]+)$/, A))
-	    N *= 99 / (99 + log(99 + A[1]))
-	if (sym ~ /^_Z[1-9]/)
-	    N *= 0.97
 	N *= RNorm
 	print N "\t" sym
     }
@@ -145,11 +118,5 @@ awk -F'\t' -v NFiles=$wsum '
 	if (N)
 	    printSym()
     }' |
-sort -g --compress-program=lz4 |tail -$((1<<20)) >out.1M
-awk -F'\t' <out.1M '$2~/^_Z/{print$2}' |
-    tail -$((64<<10)) >topsym.list
-awk -F'\t' <out.1M '$2~/^caml.*_[0-9]+$/{print$2}' |
-    tail -$((4<<10)) >>topsym.list
-awk -F'\t' <out.1M '$2!~/^_Z|^caml.*_[0-9]+$/{print$2}' |
-    tail -$((60<<10)) >>topsym.list
-sort -u -o topsym.list{,}
+sort -g --compress-program=lz4 |tail -1024 >abi.1k
+cut -f2 abi.1k |tail -256 |sort -u >topabi.list
